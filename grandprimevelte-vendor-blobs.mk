2f0de38c8f19ba5f456e2e7dad06e23c54953e84
Nemirtingas <nanaki89@hotmail.fr>
2019-01-07 23:11:11 +0100

    Updated libwvm and libWVStreamControl

--- grandprimevelte-vendor-blobs.mk
+++ grandprimevelte-vendor-blobs.mk
@@ -42,10 +42,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_MRVL.so:system/lib/egl/libGLESv1_CM_MRVL.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_MRVL.so:system/lib/egl/libGLESv2_MRVL.so \
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/lib/hw/keystore.mrvl.so:system/lib/hw/keystore.mrvl.so \
-    $(LOCAL_PATH)/proprietary/lib/libtee_client.so:system/lib/libtee_client.so
-
 # RIL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/bin/ddexe:system/bin/ddexe \
@@ -101,3 +97,71 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     $(LOCAL_PATH)/proprietary/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     $(LOCAL_PATH)/proprietary/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd 
+
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/lib/libFaceDetect.so:system/lib/libFaceDetect.so \
+    $(LOCAL_PATH)/proprietary/lib/libFlickerDetect.so:system/lib/libFlickerDetect.so \
+    $(LOCAL_PATH)/proprietary/lib/libGLSLC.so:system/lib/libGLSLC.so \
+    $(LOCAL_PATH)/proprietary/lib/libLENCMerge.so:system/lib/libLENCMerge.so \
+    $(LOCAL_PATH)/proprietary/lib/libPyramidUVDNS.so:system/lib/libPyramidUVDNS.so \
+    $(LOCAL_PATH)/proprietary/lib/libVSC.so:system/lib/libVSC.so \
+    $(LOCAL_PATH)/proprietary/lib/libarac.so:system/lib/libarac.so \
+    $(LOCAL_PATH)/proprietary/lib/libb52sensorhal.so:system/lib/libb52sensorhal.so \
+    $(LOCAL_PATH)/proprietary/lib/libcameraengine.so:system/lib/libcameraengine.so \
+    $(LOCAL_PATH)/proprietary/lib/libcoda7542.so:system/lib/libcoda7542.so \
+    $(LOCAL_PATH)/proprietary/lib/libcoda7542hal.so:system/lib/libcoda7542hal.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecaacdec.so:system/lib/libcodecaacdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecaacenc.so:system/lib/libcodecaacenc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecamrnbdec.so:system/lib/libcodecamrnbdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecamrnbenc.so:system/lib/libcodecamrnbenc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecamrwbdec.so:system/lib/libcodecamrwbdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecamrwbenc.so:system/lib/libcodecamrwbenc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodeccoda7542dec.so:system/lib/libcodeccoda7542dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodeccoda7542enc.so:system/lib/libcodeccoda7542enc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodech263dec.so:system/lib/libcodech263dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodech263enc.so:system/lib/libcodech263enc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodech264dec.so:system/lib/libcodech264dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodech264enc.so:system/lib/libcodech264enc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodech265dec.so:system/lib/libcodech265dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecjpegdec.so:system/lib/libcodecjpegdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecjpegenc.so:system/lib/libcodecjpegenc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecmp3dec.so:system/lib/libcodecmp3dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecmpeg2dec.so:system/lib/libcodecmpeg2dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecmpeg4dec.so:system/lib/libcodecmpeg4dec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecmpeg4enc.so:system/lib/libcodecmpeg4enc.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecwmadec.so:system/lib/libcodecwmadec.so \
+    $(LOCAL_PATH)/proprietary/lib/libcodecwmvdec.so:system/lib/libcodecwmvdec.so \
+    $(LOCAL_PATH)/proprietary/lib/libextsensorhal.so:system/lib/libextsensorhal.so \
+    $(LOCAL_PATH)/proprietary/lib/libgputex.so:system/lib/libgputex.so \
+    $(LOCAL_PATH)/proprietary/lib/libippimgp.so:system/lib/libippimgp.so \
+    $(LOCAL_PATH)/proprietary/lib/libippmultiexec.so:system/lib/libippmultiexec.so \
+    $(LOCAL_PATH)/proprietary/lib/libmedialib.so:system/lib/libmedialib.so \
+    $(LOCAL_PATH)/proprietary/lib/libmiscgen.so:system/lib/libmiscgen.so \
+    $(LOCAL_PATH)/proprietary/lib/libovdns.so:system/lib/libovdns.so \
+    $(LOCAL_PATH)/proprietary/lib/libsavsac.so:system/lib/libsavsac.so \
+    $(LOCAL_PATH)/proprietary/lib/libsavscmn.so:system/lib/libsavscmn.so \
+    $(LOCAL_PATH)/proprietary/lib/libsavsvc.so:system/lib/libsavsvc.so \
+    $(LOCAL_PATH)/proprietary/lib/libscenedetect.so:system/lib/libscenedetect.so \
+    $(LOCAL_PATH)/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
+    $(LOCAL_PATH)/proprietary/lib/libsnaace.so:system/lib/libsnaace.so \
+    $(LOCAL_PATH)/proprietary/lib/libsnamrnb.so:system/lib/libsnamrnb.so \
+    $(LOCAL_PATH)/proprietary/lib/libsnamrwb.so:system/lib/libsnamrwb.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomx263dsw.so:system/lib/libsomx263dsw.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomx264dsw.so:system/lib/libsomx264dsw.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxaacd.so:system/lib/libsomxaacd.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxaace.so:system/lib/libsomxaace.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxadpcmd.so:system/lib/libsomxadpcmd.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxamrd.so:system/lib/libsomxamrd.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxcmn.so:system/lib/libsomxcmn.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxcore.so:system/lib/libsomxcore.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxflacd.so:system/lib/libsomxflacd.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxhevcdsw.so:system/lib/libsomxhevcdsw.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxmp3d.so:system/lib/libsomxmp3d.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxmp4vdsw.so:system/lib/libsomxmp4vdsw.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxnaace.so:system/lib/libsomxnaace.so \
+    $(LOCAL_PATH)/proprietary/lib/libsomxvp8d.so:system/lib/libsomxvp8d.so \
+    $(LOCAL_PATH)/proprietary/lib/libsrm.so:system/lib/libsrm.so \
+    $(LOCAL_PATH)/proprietary/lib/libsxqk.so:system/lib/libsxqk.so \
+    $(LOCAL_PATH)/proprietary/lib/libstagefrighthw.so:system/lib/libstagefrighthw_stock.so\
+
